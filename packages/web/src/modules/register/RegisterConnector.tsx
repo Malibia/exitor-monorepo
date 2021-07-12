de7e3765e7c04772a5ff9c5ce48699b0ee71231a
Celestino <kcelestinomaria127@gmail.com>
2021-07-12 22:53:45 +0300

    added Apollo React and Client

---
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+// container -> view
+// container -> connector -> view
+// controller -> connector -> view
+
+
+export const RegisterConnector = () => <div>register page</div>;
