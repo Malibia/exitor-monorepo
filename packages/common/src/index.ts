8027d9206e923fb04e33f5c4551a1d48342093d3
Celestino <kcelestinomaria127@gmail.com>
2021-07-14 17:53:04 +0300

    adding field component

---
+++ packages/common/src/index.ts
@@ -0,0 +1,4 @@
+export * from './yupSchemas/user';
+
+// TO:DO must first build the common package
+// as yarn build
--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,16 +1,24 @@
 import * as React from "react";
-import { Form, Icon, Input, Button } from 'antd';
-import { withFormik, FormikErrors, FormikProps } from "formik";
-import * as yup from "yup";
-// TO:DO @types/yup
+import * as Antd from 'antd';
+import { withFormik, FormikErrors, FormikProps, Field, Form } from "formik";
+import { validUserSchema } from "@exitor-workspace/common";
+import { InputField } from "../../shared/InputField";
 
+// TO:DO yarn add -D @types/yup
 
+const { Form: AntForm, Icon, Button } = Antd;
 const FormItem = Form.item;
-
+// To add Algorand
+// Wallet Address
 interface FormValues {
     email: string;
     password: string;
 }
+/* interface FormValues {
+      email: string;
+      walletAddress: string
+}
+*/
 
 interface Props {
     submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
@@ -21,84 +29,56 @@ interface Props {
 // Wrapped with a higher-order component
 class C extends React.PureComponent<FormikProps<FormValues> & Props> {
     render() {
-        const { values, handleChange, handleBlur, handleSubmit, touched, errors } = this.props;
         return (
            <form style={{ display: "flex" }} onSubmit={handleSubmit}>
                <div style={{ width: 400, margin: "auto" }}>
-                   <FormItem
-                       help={touched.email && FormikErrors.email ? FormikErrors.email : ""}
-                       validateStatus={touched.email && errors.email ? "error" : undefined}>
-                       <input
-                         name="email"
-                         prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-                         placeholder="Email"
-                         value={values.email}
-                         onChange={handleChange}
-                         onBlur={handleBlur}
-                       />
-                     </FormItem>
-                     <FormItem
-                     help={touched.password && errors.password ? errors.password : ""}
+                   <Field 
+                    name="email"
                     // tslint:disable-next-line:jsx-no-multiline-js
-                     validateStatus={
-                        touched.password && errors.password ? "error" : undefined
-                     }>
-                       <Input
-                         name="password"
-                         prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
-                         type="password"
-                         placeholder="Password"
-                         value={values.password}
-                         onChange={handleChange}
-                         onBlur={handleBlur}
-                       />
-                     </FormItem>
-                     <FormItem>
-                       <a className="login-form-forgot" href="">
-                         Forgot password
-                       </a>
-                     </FormItem>
-                     <FormItem>
-                       <Button
-                         type="primary"
-                         htmlType="submit"
-                         className="login-form-button"
-                       >
-                         Register
-                       </Button>
-                     </FormItem>
-                     <FormItem>
-                       Or <a href="">login now!</a>
-                     </FormItem>
-                   </div>
-           </form>
+                    prefix={
+                      <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} /> as any
+                      // tslint:disable-next-line:jsx-curly-spacing
+                    }
+                    placeholder="Email"
+                    component={InputField}
+                    />
+                    <Field 
+                      name="password"
+                      type="password"
+                      // tslint:disable-next-line:jsx-no-multiline-js
+                      prefix={
+                        <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} as any
+                        // tslint:disable-next-line:jsx-curly-spacing
+                      }
+                      placeholder="Password"
+                      component={InputField}
+                      />
+                      <FormItem>
+                        <a className="login-form-forgot" href="">
+                          Forgot password
+                        </a>
+                      </FormItem>
+                      <FormItem>
+                        <Button
+                          type="primary"
+                          htmlType="submit"
+                          className="login-form-button"
+                          >
+                            Register
+
+                        </Button>
+                      </FormItem>
+                      <FormItem>
+                        Or <a href="">login now!</a>
+                      </FormItem>
+                      </div>
+                      </form>
         );
     }
 }
 
-const emailNotLongEnough = "email must be at least 3 characters";
-const passwordNotLongEnough = "password must be at least 3 characters";
-const invalidEmail = "email must be a valid email";
-
-
-const validationSchema = yup.object().shape({
-    email: yup
-      .string()
-      .min(3, emailNotLongEnough)
-      .max(255)
-      .email(invalidEmail)
-      .required(),
-    password: yup
-      .string()
-      .min(3, passwordNotLongEnough)
-      .max(255)
-      .required()
-  });
-  
-
-
 export const RegisterView = withFormik<FormikProps, FormValues>({
-    validationSchema,
+    validationSchema: validUserSchema,
     //validateOnChange: false,
     //validateOnBlur: false,
     mapPropsToValues: () => ({ email: "", password: ""}),
